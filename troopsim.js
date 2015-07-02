0789ab7244a39e8e26f9bb67aa88141d30414d0e
Jeremy Goldberg <elias.goldberg@gmail.com>
2015-07-02 19:28:16 -0400

    added resource training costs.

--- troopsim.js
+++ troopsim.js
@@ -1,4 +1,32 @@
 angular.module('troopSim',[])
 .controller('troopSimCtrl', function($scope) {
-    $scope.msg = 'troop simulator';
+    $scope.resourceTrainingCosts = [
+      { tier: 1,
+        costs: [
+          {name: 'Cavalry', percent: 0},
+          {name: 'Range', percent: 0},
+          {name: 'Infantry', percent: 0},
+          {name: 'Siege', percent: 0}
+        ]},
+      { tier: 2,
+        costs: [
+          {name: 'Cavalry', percent: 0},
+          {name: 'Range', percent: 0},
+          {name: 'Infantry', percent: 0},
+          {name: 'Siege', percent: 0}
+      ]},
+      { tier: 3,
+        costs: [
+          {name: 'Cavalry', percent: 0},
+          {name: 'Range', percent: 0},
+          {name: 'Infantry', percent: 0},
+          {name: 'Siege', percent: 0}
+      ]},
+      { tier: 4,
+        costs: [
+          {name: 'Cavalry', percent: 0},
+          {name: 'Range', percent: 0},
+          {name: 'Infantry', percent: 0},
+          {name: 'Siege', percent: 0}
+      ]}];
   });
